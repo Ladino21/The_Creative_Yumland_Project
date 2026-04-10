fd84e3054a68edcd8e75287074e94903c19977c8
Ladino21 <n.mekeddem@hotmail.com>
2026-04-10 17:15:54 +0200

    Add session verification functions

---
+++ include /session.php
@@ -0,0 +1,18 @@
+<?php
+// Verif connexion
+function verifier_session(){
+    if(empty($_SESSION["email"])){
+        header("location: ../connexion.php");
+        exit;
+    }
+}
+
+// Verif bon rôle (pour admin.php)
+function verifier_role($role_requis){
+    verifier_session();
+    if($_SESSION["role"] !== $role_requis){
+        header("location: ../connexion.php");
+        exit;
+    }
+}
+?>
