d779e8ee585033776a9fbd8147ee6eddb8e1dc30
Ladino21 <n.mekeddem@hotmail.com>
2026-04-10 17:18:50 +0200

    update

--- include /session.php
+++ include /session.php
@@ -2,7 +2,7 @@
 // Verif connexion
 function verifier_session(){
     if(empty($_SESSION["email"])){
-        header("location: ../connexion.php");
+        header("location:connexion.php");
         exit;
     }
 }
@@ -10,8 +10,8 @@ function verifier_session(){
 // Verif bon rôle (pour admin.php)
 function verifier_role($role_requis){
     verifier_session();
-    if($_SESSION["role"] !== $role_requis){
-        header("location: ../connexion.php");
+    if($_SESSION["role"]!==$role_requis){
+        header("location:connexion.php");
         exit;
     }
 }
